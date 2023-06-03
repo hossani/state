0fedfbf98085e1af7fb2792afa24257c1441b8c0
hossani <hamza.hossani.001@gmail.com>
2023-06-03 16:14:32 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import image from './profile-image.jpg';
+import './App.css'
+class App extends Component {  
+ // Définition de l'état initial
+  state = {
+    person: {
+      fullName: 'Hamza Hossani',
+      bio: 'Bio: Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      imgSrc: image,
+      profession: 'Profession: Engineer'
+    },
+    show: false,
+    lastMountedTime: null
+  };
+// Méthode du cycle de vie
+  componentDidMount() {
+    this.setState({ lastMountedTime: new Date() });
+    this.interval = setInterval(() => {
+      this.setState({ lastMountedTime: new Date() });
+    }, 1000);
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+// Fonction pour formater l'intervalle de temps
+  formatTimeInterval = (interval) => {
+    const seconds = Math.floor((interval / 1000) % 60);
+    const minutes = Math.floor((interval / (1000 * 60)) % 60);
+    const hours = Math.floor((interval / (1000 * 60 * 60)) % 24);
+
+    return `${hours}h ${minutes}m ${seconds}s`;
+  };
+
+   // Méthode de rendu pour définir l'interface utilisateur du composant
+  render() {
+    const { person, show, lastMountedTime } = this.state;
+
+    return (
+      <div className="app-container">
+        {show && (
+          <div className="profile">
+            <h2>{person.fullName}</h2>
+            <p>{person.bio}</p>
+            <img src={person.imgSrc} alt="image" width="350" height="350" />
+            <p>{person.profession}</p>
+          </div>
+        )}
+        <button onClick={() => this.setState({ show: !show })}>
+          Toggle Profile
+        </button>
+        {lastMountedTime && (
+          <p>
+            Last mounted: {this.formatTimeInterval(new Date() - lastMountedTime)}
+          </p>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
